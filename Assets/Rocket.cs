affb9158c0745eca71b23826a753cfc39f762b2c
DESKTOP-4QB4TVC\Diego Belini <drxbelini@gmail.com>
2018-07-31 00:41:10 -0300

    Set collision behaver to send a message and make a first level desig.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,48 +5,77 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+   [SerializeField] float rcsThrust = 100f;
+   [SerializeField] float engThrust = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
    
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        ProcessInput();                
+        audioSource = GetComponent<AudioSource>();        
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
 
+            case "Fuel":
+                print("Fuel");
+                break;
+
+            default:
+                print("dead");
+                break;
+                
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        Thrust();
+        Rotate();        
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space)) //When space is pessed vector3 get UP force. And engine sound FX start play
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            PlayRocketEngineFX();
+            rigidBody.AddRelativeForce(Vector3.up * engThrust);
+            if (!audioSource.isPlaying) //Start to play audio when the space key is pressed and the audio is not play yet
+            {
+                audioSource.Play();
+            }
         }
         else //Stop rocket sounf FX
         {
-             audioSource.Stop();
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * Time.deltaTime * 75);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * 75);
-        }       
+            audioSource.Stop();
+        }      
     }
 
-    private void PlayRocketEngineFX()
+    private void Rotate()
     {
-        if (!audioSource.isPlaying) //Start to play audio when the space key is pressed and the audio is not play yet
+        rigidBody.freezeRotation = true;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            audioSource.Play();
-        }       
+            transform.Rotate(Vector3.forward * Time.deltaTime * rcsThrust);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * rcsThrust);
+        }
+
+        rigidBody.freezeRotation = false;
     }
+
+
+   
 }
